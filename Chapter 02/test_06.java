422d53747f7bf987e1ec790fcc5934696216cd7c
gangintheremark <nkh9587@naver.com>
2022-01-12 16:28:19 +0900

    자바 기본 프로그래밍

--- Chapter 02/test_06.java
+++ Chapter 02/test_06.java
@@ -4,7 +4,7 @@ public class test_06 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 
-		System.out.print("1~99 ������ ������ �Է��Ͻÿ�>>");
+		System.out.print("1~99 사이의 정수를 입력하시오>>");
 		int index = s.nextInt();
 
 		int a = index / 10;
@@ -12,11 +12,11 @@ public class test_06 {
 
 		if (a % 3 == 0)
 			if (b % 3 == 0)
-				System.out.println("�ڼ�¦¦");
+				System.out.println("박수짝짝");
 			else
-				System.out.print("�ڼ�¦");
+				System.out.print("박수짝");
 		else if (b % 3 == 0)
-			System.out.println("�ڼ�¦");
+			System.out.println("박수짝");
 
 		s.close();
 
